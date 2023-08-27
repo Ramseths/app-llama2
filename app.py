12a57484b8b5bda6a1a07e9322e198d62987673c
Ramseths <jesus.ramseths@gmail.com>
2023-08-26 18:04:27 -0600

    feat: add layout

--- app.py
+++ app.py
@@ -11,17 +11,19 @@ def greet(name):
 
 languages = ['JavaScript', 'Python', 'C++']
 functions = ['Rosenbrock', 'Rastrigin']
-algorithm = ['Particle Swarm Optimization', 'Artificial Bee Colony', 'Differential Evolution']
+algorithms = ['Particle Swarm Optimization', 'Artificial Bee Colony', 'Differential Evolution']
 
 with gr.Blocks() as demo:
     with gr.Row():
-        name = gr.Label('Code Generation 💻')
+        title = gr.Label('Evolutionary Computation - Code Generation 🧬💻')
     with gr.Row():
         textArea = gr.Textbox(label='Enter the code topic:', lines = 4)
     with gr.Row():
-        language = gr.Dropdown(languages, label = '', multiselect=False)
-        function = gr.Dropdown(languages, multiselect=False)
+        language = gr.Dropdown(languages, label = 'Programming Languages', multiselect=False)
+        function = gr.Dropdown(functions, label = 'Function', multiselect=False)
+        algorithm = gr.Dropdown(algorithms, label = 'Algorithms', multiselect=False)
     with gr.Row():
+        btn_generate = gr.Button("Generate")
         with gr.Column(scale=1, min_width=600):
             text1 = gr.Textbox(label="prompt 1")
             text2 = gr.Textbox(label="prompt 2")

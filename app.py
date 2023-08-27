54af9da483a75b6a0a19abd38379ef60a1780363
Ramseths <jesus.ramseths@gmail.com>
2023-08-26 21:56:40 -0600

    fix: model app

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import gradio as gr
 from model import getResponse
 
-def greet(name):
-    return "Hello " + name + "!"
-
 genres = ['Romance', 'Comedy', 'Horror', 'Fantasy']
-
 
 with gr.Blocks() as demo:
     with gr.Row():
         title = gr.Label('Story Generation with Llama 2 📝💻')
     with gr.Row():
-        story = gr.Textbox(label='Enter the Story Topic:', lines = 2)
+        story = gr.Textbox(label = 'Enter the Story Topic:', lines=2)
     with gr.Row():
-        genre = gr.Dropdown(genres, label = 'Story Genre', multiselect=False)
-        character = gr.Textbox(placeholder = 'Name',label = 'Main Character')
+        genre = gr.Dropdown(genres, label = 'Story Genre', multiselect = False)
+        character = gr.Textbox(placeholder='Name', label='Main Character')
     with gr.Row():
         output = gr.Textbox(label = 'Story:')
         btn = gr.Button("Generate")
-        btn.click(fn = getResponse, inputs = [story, genre, character], outputs = output)
-            
+        btn.click(fn = getResponse, inputs=[story, genre, character], outputs=output)
+
 demo.launch()
--- model.py
+++ model.py
@@ -1,25 +1,24 @@
-
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 
 def getResponse(story_topic, genre, main_character):
-    llm = CTransformers(model='./models/llama-2-7b-chat.ggmlv3.q8_0.bin',
-                    model_type='llama',
-                    config={'max_new_tokens': 256,
-                            'temperature': 0.01})
+
+    llm = CTransformers(model = "./models/llama-2-7b-chat.ggmlv3.q8_0.bin",
+                        model_type="llama",
+                        config={'max_new_tokens' : 256,
+                                'temperature' : 0.01})
     
-    print('Building template...')
+    print('Template')
     template = """
-    Write a story of the genre {genre} and include the topic of:{story_topic} with the main character {main_character}:
+    Write a story of the genre {genre} and include the topic of: {story_topic} with the main character {main_character}:
     """
 
     prompt = PromptTemplate(
-    input_variables=['story_topic', 'genre', 'main_character'],
-    template=template,)
+        input_variables=['story_topic', 'genre', 'main_character'],
+        template=template
+    )
 
-  
-    # Generating the response using LLaMa 2
-    response=llm(prompt.format(story_topic = story_topic, genre = genre, main_character = main_character))
+    response = llm(prompt.format(story_topic = story_topic, genre = genre, main_character = main_character))
     print(response)
 
     return response
